5d771ce70992851f404a820d06e7c7d430f62dcc
Satyam Bhardwaj <satyam.bhardwaj@decatem.com>
2020-07-02 15:11:31 +0530

    language changes

--- js/language.js
+++ js/language.js
@@ -1,61 +1,36 @@
-$(document).hide();
-
-$('[lang]').hide(); // hide all lang attributes on start.
-$('[lang="es"]').show(); 
-$('#language').click(function () { // put onchange event when user select option from select
-    var lang = $(this).html(); // decide which language to display using switch case. The rest is obvious (i think)
-    switch (lang) {
-        case 'English':
-            $('[lang]').hide();
-            $('[lang="en"]').show();
-        break;
-        case 'Español':
-            $('[lang]').hide();
-            $('[lang="es"]').show();
-        break;
-        default:
-            $('[lang]').hide();
-            $('[lang="es"]').show();
-        }
-});
-
-$('#language-mobile').click(function () { // put onchange event when user select option from select
-    var lang = $(this).html(); // decide which language to display using switch case. The rest is obvious (i think)
-    switch (lang) {
-        case 'English':
-            $('[lang]').hide();
-            $('[lang="en"]').show();
-        break;
-        case 'Español':
-            $('[lang]').hide();
-            $('[lang="es"]').show();
-        break;
-        default:
-            $('[lang]').hide();
-            $('[lang="es"]').show();
-        }
-});
-
-function changeLanguage() {
-    console.log("test");
-    if(document.getElementById("language").innerHTML === "Español") {
-        document.getElementById("language").innerHTML = "English";
-        document.getElementById("language-mobile").innerHTML = "English";
-    }
-    else {
-        document.getElementById("language").innerHTML = "Español";
-        document.getElementById("language-mobile").innerHTML = "Español";
-    }
-}
-
-function changeLanguageMobile() {
-    console.log("test");
-    if(document.getElementById("language-mobile").innerHTML !== "Español") {
-        document.getElementById("language-mobile").innerHTML = "English";
-        document.getElementById("language").innerHTML = "English";
-    }
-    else {
-        document.getElementById("language-mobile").innerHTML = "Español";
-        document.getElementById("language").innerHTML = "Español";
-    }
-}
+$(document).ready(function(){
+  
+    // By default
+    $('.en_lang').addClass("active-lang");
+    $('#lang-switch .en').addClass("active-flag");
+    
+    // Function switch
+    $(function() {
+      // Spanish button
+      $("#lang-switch .es").click(function() {
+        // Enable Spanish
+        $('.es_lang').addClass("active-lang"); 
+        
+        // Disable English
+        $('.en_lang').removeClass("active-lang") 
+        
+        // Active or remove the opacity on the flags.
+        $('#lang-switch .es').addClass("active-flag");
+        $('#lang-switch .en').removeClass("active-flag");
+      });
+      
+      // English button
+      $("#lang-switch .en").click(function() {
+        
+        // Enable English
+        $('.en_lang').addClass("active-lang");
+        
+        // Disable Spanish
+        $('.es_lang').removeClass("active-lang")
+        
+        // Active or remove the opacity on the flags.
+        $('#lang-switch .en').addClass("active-flag");
+        $('#lang-switch .es').removeClass("active-flag");
+      });
+    });
+  });
